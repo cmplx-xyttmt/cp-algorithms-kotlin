fea7c739507af7546fe28efecb23647e97b6985e
cmplx-xyttmt <archimedesisaac@gmail.com>
2019-08-23 10:30:32 +0300

    Add 2 functions (preComputePows and prefixHashes) to prevent repetitive code

--- src/main/kotlin/strings/applications.kt
+++ src/main/kotlin/strings/applications.kt
@@ -4,16 +4,11 @@ package strings
  * Application of hashing: Calculating number of different substrings of a string in O(n^2log n).
  */
 fun computeUniqueSubstrings(string: String): Int {
-    val p = 31
     val m = (1e9 + 9).toInt()
     val n = string.length
-    val pPows = LongArray(n)
-    pPows[0] = 1
-    for (i in 1 until pPows.size) pPows[i] = (pPows[i - 1] * p) % m
+    val pPows = preComputePowers(n, m)
 
-    // prefixHash[i] is the hash of the prefix of string with i characters
-    val prefixHash = LongArray(n + 1)
-    for (i in 0 until n) prefixHash[i + 1] = (prefixHash[i] + (string[i] - 'a' + 1) * pPows[i]) % m
+    val prefixHash = prefixHashes(string, pPows, m)
 
     var count = 0
     for (l in 1..n) {
--- src/main/kotlin/strings/useful_functions.kt
+++ src/main/kotlin/strings/useful_functions.kt
@@ -11,18 +11,42 @@ package strings
  *  - Calculating number of different substrings of a string in O(n^2log n).
  *  - Calculating number of palindromic substrings in a string.
  *
+ * An easy trick for reducing the probability of collision is to use 2 different hashes (i.e use different p and/or m)
+ *
  * @param string: the string to be hashed.
  * @param p: a prime that is roughly equal to the number of characters in the input alphabet. Default value is 31 assuming an alphabet containing only lowercase english characters, need to update depending on size of alphabet.
  * @param m: modulus (probability of hash collision is 1/m), a good choice for this is a large prime number. Default value is 1e9 + 9.
  */
-fun computeHash(string: String, p: Int = 31, m: Int = (1e9 + 9).toInt()): Long {
+fun computeHash(string: String, m: Int = (1e9 + 9).toInt()): Long {
     var hashValue = 0L
-    var pPow = 1L
+    val pPow = preComputePowers(string.length, m)
 
-    string.forEach { c ->
-        hashValue = (hashValue + (c - 'a' + 1) * pPow) % m
-        pPow = (pPow * p) % m
+    string.forEachIndexed { i, c ->
+        hashValue = (hashValue + (c - 'a' + 1) * pPow[i]) % m
     }
 
     return hashValue
+}
+
+/**
+ * Returns an array of powers of p modulo m
+ */
+fun preComputePowers(size: Int, m: Int, p: Int = 31): LongArray {
+    val pPow = LongArray(size)
+    pPow[0] = 1
+    for (i in 1 until pPow.size) pPow[i] = (pPow[i - 1] * p) % m
+
+    return pPow
+}
+
+/**
+ * Computes the hashes of the prefixes of the string.
+ */
+fun prefixHashes(string: String, pPows: LongArray, m: Int): LongArray {
+    val n = string.length
+    // prefixHash[i] is the hash of the prefix of string with i characters
+    val prefixHash = LongArray(n + 1)
+    for (i in 0 until n) prefixHash[i + 1] = (prefixHash[i] + (string[i] - 'a' + 1) * pPows[i]) % m
+
+    return prefixHash
 }

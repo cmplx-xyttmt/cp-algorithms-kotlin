43d095542df2b5046680e40f69a0a01e2d63cb43
cmplx-xyttmt <archimedesisaac@gmail.com>
2019-10-24 14:30:58 +0300

    Implement permutations

--- src/main/kotlin/recursion/useful_functions.kt
+++ src/main/kotlin/recursion/useful_functions.kt
@@ -21,3 +21,31 @@ fun subsets(n: Int): List<List<Int>> {
     subsets.sortBy { it.size }
     return subsets.toList()
 }
+
+/**
+ * Returns all permutations of the set {1, 2, ..., n}
+ */
+fun permutations(n: Int): List<List<Int>> {
+    val permutations = mutableListOf<List<Int>>()
+    val perm = mutableListOf<Int>()
+    val chosen = MutableList(n + 1) { false }
+
+    fun permutationHelper() {
+        if (perm.size == n) permutations.add(perm.toList())
+        else {
+            (1..n).forEach {
+                if (!chosen[it]) {
+                    chosen[it] = true
+                    perm.add(it)
+                    permutationHelper()
+                    perm.remove(it)
+                    chosen[it] = false
+                }
+            }
+        }
+    }
+
+    permutationHelper()
+
+    return permutations.toList()
+}
--- src/test/kotlin/recursion/TestUsefulFunctions.kt
+++ src/test/kotlin/recursion/TestUsefulFunctions.kt
@@ -12,4 +12,12 @@ class TestUsefulFunctions {
         assertEquals(listOf(listOf(), listOf(1), listOf(2), listOf(3), listOf(1, 2),
             listOf(1, 3), listOf(2, 3), listOf(1, 2, 3)), subsets(3))
     }
+
+    @Test fun testPermutations() {
+        assertEquals(listOf(listOf()), permutations(0))
+        assertEquals(listOf(listOf(1)), permutations(1))
+        assertEquals(listOf(listOf(1, 2), listOf(2, 1)), permutations(2))
+        assertEquals(listOf(listOf(1, 2, 3), listOf(1, 3, 2), listOf(2, 1, 3), listOf(2, 3, 1),
+            listOf(3, 1, 2), listOf(3, 2, 1)), permutations(3))
+    }
 }

c78399500abe4bb82fce3cf302911006591a3b62
Isaac Owomugisha <archimedesisaac@gmail.com>
2020-07-29 18:08:51 +0300

    Further improve the suffix array implementation such that counting sort (instead of radix sort) is needed.

--- src/main/kotlin/strings/suffix_array.kt
+++ src/main/kotlin/strings/suffix_array.kt
@@ -1,7 +1,5 @@
 package strings
 
-import kotlin.math.max
-
 /**
  * Creates the suffix array of the string s. For more info, checkout the EDU section on codeforces
  * Note: A suffix array is a list of all suffixes of a string in lexicographic order.
@@ -12,13 +10,13 @@ import kotlin.math.max
 fun suffixArray(inputString: String): List<Int> {
     val s = "$inputString$"
     val n = s.length
-    var p: List<Int> // Ordering of the strings
-    val c = MutableList(n) { -1 } // Equivalence classes
+    var p: IntArray // Ordering of the strings
+    val c = IntArray(n) { -1 } // Equivalence classes
 
     // k = 0
     val charPosition = s.mapIndexed { index, ch -> Pair(ch, index) }.sortedBy { it.first }
 
-    p = charPosition.map { it.second }
+    p = charPosition.map { it.second }.toIntArray()
 
     charPosition.forEachIndexed { index, pair ->
         when {
@@ -28,49 +26,39 @@ fun suffixArray(inputString: String): List<Int> {
         }
     }
 
+    fun countSort(ordering: IntArray, classes: IntArray): IntArray {
+        val newOrdering = IntArray(n)
+        val counts = IntArray(n)
+        classes.forEach { `class` -> counts[`class`]++ }
+
+        val position = IntArray(n)
+        for (i in 1 until n) position[i] = position[i - 1] + counts[i - 1]
+
+        ordering.forEach { index ->
+            val `class` = classes[index]
+            newOrdering[position[`class`]] = index
+            position[`class`]++
+        }
+        return newOrdering
+    }
+
     var k = 0
     while (1 shl k < n) {
         // k -> k + 1
-        val eqPositionPair = radixSort(MutableList(n) { index ->
-            Pair(Pair(c[index], c[(index + (1 shl k)) % n]), index)
-        })
 
-        p = eqPositionPair.map { it.second }
+        for (i in p.indices) p[i] = (p[i] - (1 shl k) + n) % n
 
-        eqPositionPair.forEachIndexed { index, pair ->
-            when {
-                index == 0 -> c[p[index]] = 0
-                pair.first == eqPositionPair[index - 1].first -> c[p[index]] = c[p[index - 1]]
-                else -> c[p[index]] = c[p[index - 1]] + 1
-            }
+        p = countSort(p, c)
+        val cNew = IntArray(n)
+        cNew[p[0]] = 0
+        for (i in 1 until n) {
+            val prev = Pair(c[p[i - 1]], c[(p[i - 1] + (1 shl k)) % n])
+            val curr = Pair(c[p[i]], c[(p[i] + (1 shl k)) % n])
+            cNew[p[i]] = if (prev == curr) cNew[p[i - 1]] else cNew[p[i - 1]] + 1
         }
-
+        cNew.forEachIndexed { index, `class` -> c[index] = `class` }
         k++
     }
 
-    return p
-}
-
-/**
- * Implements radix sort on a list of pairs.
- *
- * @param pairs list to be sorted
- * @return a sorted version of the pairs list
- */
-fun radixSort(pairs: List<Pair<Pair<Int, Int>, Int>>): List<Pair<Pair<Int, Int>, Int>> {
-    val maxNum = pairs.map { pair -> max(pair.first.first, pair.first.second) }.max()!!
-
-    fun sortPairBy(first: Boolean, pairsToSort: List<Pair<Pair<Int, Int>, Int>>): List<Pair<Pair<Int, Int>, Int>> {
-        val buckets = MutableList(maxNum + 1) { mutableListOf<Pair<Pair<Int, Int>, Int>>() }
-
-        pairsToSort.forEach { pair -> buckets[if (first) pair.first.first else pair.first.second].add(pair) }
-
-        val sortedPairs = mutableListOf<Pair<Pair<Int, Int>, Int>>()
-        buckets.forEach { bPairs -> sortedPairs.addAll(bPairs) }
-
-        return sortedPairs
-    }
-
-    val sortedBySecond = sortPairBy(false, pairs)
-    return sortPairBy(true, sortedBySecond)
+    return p.toList()
 }

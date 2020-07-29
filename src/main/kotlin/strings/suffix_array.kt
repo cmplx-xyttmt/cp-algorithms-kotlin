1bbb8d88997a303860bf08283786fee3363cfd40
Isaac Owomugisha <archimedesisaac@gmail.com>
2020-07-29 17:40:04 +0300

    Improve suffix array implementation by using radix sort

--- src/main/kotlin/strings/suffix_array.kt
+++ src/main/kotlin/strings/suffix_array.kt
@@ -1,13 +1,16 @@
 package strings
 
+import kotlin.math.max
+
 /**
  * Creates the suffix array of the string s. For more info, checkout the EDU section on codeforces
+ * Note: A suffix array is a list of all suffixes of a string in lexicographic order.
  *
- * @param ss the string whose suffix array is to be created
+ * @param inputString the string whose suffix array is to be created
  * @return the suffix array
  */
-fun suffixArray(ss: String): List<Int> {
-    val s = "$ss$"
+fun suffixArray(inputString: String): List<Int> {
+    val s = "$inputString$"
     val n = s.length
     var p: List<Int> // Ordering of the strings
     val c = MutableList(n) { -1 } // Equivalence classes
@@ -28,9 +31,9 @@ fun suffixArray(ss: String): List<Int> {
     var k = 0
     while (1 shl k < n) {
         // k -> k + 1
-        val eqPositionPair = MutableList(n) {
-                index ->  Pair(ComparablePair(c[index], c[(index + (1 shl k)) % n]), index)
-        }.sortedBy { it.first }
+        val eqPositionPair = radixSort(MutableList(n) { index ->
+            Pair(Pair(c[index], c[(index + (1 shl k)) % n]), index)
+        })
 
         p = eqPositionPair.map { it.second }
 
@@ -48,14 +51,26 @@ fun suffixArray(ss: String): List<Int> {
     return p
 }
 
-data class ComparablePair(private val first: Int, private val second: Int): Comparable<ComparablePair> {
+/**
+ * Implements radix sort on a list of pairs.
+ *
+ * @param pairs list to be sorted
+ * @return a sorted version of the pairs list
+ */
+fun radixSort(pairs: List<Pair<Pair<Int, Int>, Int>>): List<Pair<Pair<Int, Int>, Int>> {
+    val maxNum = pairs.map { pair -> max(pair.first.first, pair.first.second) }.max()!!
 
-    override fun compareTo(other: ComparablePair): Int {
-        if (first == other.first) return second - other.second
-        return first - other.first
+    fun sortPairBy(first: Boolean, pairsToSort: List<Pair<Pair<Int, Int>, Int>>): List<Pair<Pair<Int, Int>, Int>> {
+        val buckets = MutableList(maxNum + 1) { mutableListOf<Pair<Pair<Int, Int>, Int>>() }
+
+        pairsToSort.forEach { pair -> buckets[if (first) pair.first.first else pair.first.second].add(pair) }
+
+        val sortedPairs = mutableListOf<Pair<Pair<Int, Int>, Int>>()
+        buckets.forEach { bPairs -> sortedPairs.addAll(bPairs) }
+
+        return sortedPairs
     }
 
-    override fun toString(): String {
-        return "Pair($first, $second)"
-    }
+    val sortedBySecond = sortPairBy(false, pairs)
+    return sortPairBy(true, sortedBySecond)
 }
